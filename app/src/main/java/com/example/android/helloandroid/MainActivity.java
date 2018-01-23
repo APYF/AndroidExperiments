d37b7ed78c72067389356838d299ab60f7d56b79
AF_BA <amypyfong@gmail.com>
2018-01-23 01:03:42 -0800

    new repo

---
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.android.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng udacity = new LatLng(37.3998046, -122.1105261);
+        googleMap.addMarker(new MarkerOptions().position(udacity).title("Udacity"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(udacity, 15));
+    }
+}
+
